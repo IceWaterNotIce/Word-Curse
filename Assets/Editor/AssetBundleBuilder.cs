25ff5aacbdfae79a4528307e7bdd58cb0c49f81f
冰水不冰 <icewaternotice@gmail.com>
2025-01-05 23:37:18 +0800

    Auto commit from Unity Asset Bundle Builder

--- Assets/Editor/AssetBundleBuilder.cs
+++ Assets/Editor/AssetBundleBuilder.cs
@@ -15,8 +15,8 @@ public class AssetBundleBuilder
             BuildTarget.StandaloneWindows
         );
 
-        // UpdateVersionJson();
-        // CommitAndPushToGit();
+        UpdateVersionJson();
+        CommitAndPushToGit();
     }
 
 
