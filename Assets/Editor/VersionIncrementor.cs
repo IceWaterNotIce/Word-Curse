4064c14ecc6328afdf3fab7d678ea1af958663b7
冰水不冰 <icewaternotice@gmail.com>
2025-01-11 18:10:58 +0800

    Auto commit from Unity Builder.

--- Assets/Editor/VersionIncrementor.cs
+++ Assets/Editor/VersionIncrementor.cs
@@ -177,7 +177,9 @@ public class VersionIncrementor : IPreprocessBuildWithReport
             // 獲取子文件夾名稱並創建對應的 FTP 文件夾 URL
             string directoryName = Path.GetFileName(directory).Replace("\\", "/").Replace("ftp:/", "ftp://");
             string newFtpUrl = $"{ftpUrl}{directoryName}/";
-
+            string ftpDirectory = Path.GetDirectoryName(directoryName).Replace("\\", "/").Replace("ftp:/", "ftp://") + "/";
+            UnityEngine.Debug.Log($"ftpDirectory: {ftpDirectory}");
+            CreateFtpDirectory(ftpDirectory, account.username, account.password);
             // 遞歸上傳子文件夾中的文件
             UploadFilesRecursively(directory, newFtpUrl, account);
         }
@@ -190,9 +192,7 @@ public class VersionIncrementor : IPreprocessBuildWithReport
             UnityEngine.Debug.Log($"File does not exist: {localFilePath}");
             return;
         }
-        string ftpDirectory = Path.GetDirectoryName(ftpUrl).Replace("\\", "/").Replace("ftp:/", "ftp://") + "/";
-        UnityEngine.Debug.Log($"ftpDirectory: {ftpDirectory}");
-        CreateFtpDirectory(ftpDirectory, username, password);
+
 
         FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpUrl);
         request.Method = WebRequestMethods.Ftp.UploadFile;
